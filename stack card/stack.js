f95c7fc4c69797389e50fa1e1830c838cb09ed4a
jfruit <jfruitty.regis@gmail.com>
2021-10-13 12:04:35 +0700

    stackcard

--- stack card/stack.js
+++ stack card/stack.js
@@ -2,15 +2,23 @@ let stack = document.querySelector(".stack");
 
 [...stack.children].reverse().forEach(i => stack.append(i));
 
-stack.addEventListener("click", swap);
 
-function swap(e) {
-  let card = document.querySelector(".card:last-child");
-  if (e.target !== card) return;
-  card.style.animation = "swap 700ms forwards";
-
+function forward(e) {
+  console.log(e);
+  let card = document.querySelector(".stackcard:last-child");
+  card.style.animation = "next 1500ms forwards";
   setTimeout(() => {
     card.style.animation = "";
     stack.prepend(card);
-  }, 700);
+  }, 1000);
+}
+
+function backward(e) {
+  console.log(e);
+  let first = document.querySelector(".stackcard:first-child");
+  first.style.animation = "swap 1500ms forwards";
+  setTimeout(() => {
+    first.style.animation = "";
+    stack.append(first);
+  }, 1000);
 }
